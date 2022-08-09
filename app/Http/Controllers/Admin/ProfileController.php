26e361ab38eff93d6d547638233673595ed10fd1
nobuyuki nagato <n.nagagto726@gmail.com>
2022-08-09 12:01:07 +0000

    first

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,18 +4,43 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\profile;
 class ProfileController extends Controller
 {
     //
     
-        public function add()
+    public function add()
     {
         return view('admin.profile.create');
     }
 
-    public function create()
+            // 以下を追記
+    public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request, profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $profile->image_path = basename($path);
+        } else {
+            $profile->image_path = null;
+        }
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();  
+    
+ 
         return redirect('admin/profile/create');
     }
 
---
+++ database/migrations/2022_08_09_112726_create_profile_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('gender');
+            $table->string('hobby')->nullable();
+            $table->string('introduction')->nullable();
+            $table->string('image_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}

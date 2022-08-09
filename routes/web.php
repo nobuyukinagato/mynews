9644d6a6f5f361a44c2ecd7e99e92d622554a9dd
nobuyuki nagato <n.nagagto726@gmail.com>
2022-08-09 05:59:33 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
 
-    Route::get('news/create', 'Admin\NewsController@add');
-});
+ 
+// // ■コラム
+// ログインしていない場合のリダイレクト処理に、middlewareを利用します。
+// middlewareの利用は、下記のサイトを参考にするといいです。
+
+// https://readouble.com/laravel/6.x/ja/middleware.html
+
+
 
 // 課題4
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create');
+});
+// Route::group(['prefix' => 'admin'], function() {
+
+//     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');;
+// });
+
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('Profile/create', 'Admin\ProfileController@add');
-    Route::get('Profile/edit', 'Admin\ProfileController@edit');  
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');;
     });
 
 // 課題3
 // Route::get('XXX','AAAController@bbb');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
